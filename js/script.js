4f2a35940c50d3525bec714e17315368bf3e5167
eihwazwunjo <eihwazwunjo@gmail.com>
2025-08-30 13:24:28 +0900

    프로젝트 생성 및 기존 파일 업로드

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    // setInterval(function(){
+
+    //     $('.imgbox').animate({
+    //         marginLeft: '-100%'
+    //     },1000, function(){
+    //         $('.imgbox img').eq(0).appendTo('.imgbox');
+    //     });
+    // },6000);
+
+    
+    // setInterval(function(){
+
+    //     $('.imgbox').animate({
+    //         marginLeft: '-100%'
+    //     },1000, function(){
+    //         $('.imgbox img').eq(0).appendTo('.imgbox');
+    //         $('.imgbox').css({
+    //             marginLeft: 0
+    //         });
+    //     });
+    // },6000);
+
+});
